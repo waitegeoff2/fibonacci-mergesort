093eadb8bfbf5e3e4f6821ba8e115959d7acea2b
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-20 16:30:33 -0400

    Clarify merge sort

--- merge.js
+++ merge.js
@@ -1,20 +1,23 @@
 function merge(left, right) {
-    let sortedArr = [] // the sorted items will go here
+    let sortedArr = [] // sorted items go here
     while (left.length && right.length) {
       // Insert the smallest item into sortedArr
       if (left[0] < right[0]) {
         sortedArr.push(left.shift())
+        //if the number at index 0 on the left is smaller than the right one, push it into the array
       } else {
         sortedArr.push(right.shift())
+        //or else, if the index 0 on the right is, push it in
       }
     }
-    // Use spread operators to create a new array, combining the three arrays
+    // Use spread operators to create a new array, combining three arrays
     return [...sortedArr, ...left, ...right]
 }
 
 function mergeSort(arr) {
     // Base case
     if (arr.length <= 1) return arr
+    //splitting it in half
     let mid = Math.floor(arr.length / 2)
     // Recursive calls
     let left = mergeSort(arr.slice(0, mid))
